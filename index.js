d0aa60f87bd7e26530d350627d6036f70894b145
Bayraa1204 <Bayraasworldchinerdene@gmail.com>
2024-08-19 17:50:31 +0800

    hey

--- index.js
+++ index.js
@@ -5,10 +5,15 @@ const checkBox = document.getElementById("checkbox");
 
 const btn = document.getElementById("button");
 
+let isItTrue = false;
+
+let isGood = 0
+
 function isValidEmail() {
   const isEmailValid = email.value;
   if (isEmailValid.includes("@")) {
-    alert("Email is Valid");
+    alert("Perfect");
+    isGood += 1
   } else {
     alert("Email is Invalid");
   }
@@ -16,11 +21,14 @@ function isValidEmail() {
 
 function isValidPhoneNum() {
   const isPhoneValid = phoneNumber.value;
-  if (NaN(isPhoneValid)) {
-    if (isPhoneValid.length >= 8) {
-      alert("Phone number is Valid");
-    } else {
+  if (itIncludes(isPhoneValid)) {
+    if (isPhoneValid.length < 8) {
       alert("Phone number is Too Short");
+    } else if (isPhoneValid.length > 8) {
+      alert("Phone number is Too Long");
+    } else {
+      alert("Perfect");
+      isGood += 1
     }
   } else {
     alert("Phone number is Invalid");
@@ -29,17 +37,28 @@ function isValidPhoneNum() {
 
 function isValidPass() {
   const isPassValid = password.value;
-  if (isPassValid.length >= 8) {
-    alert("Password is Valid");
-  } else {
+  if (isPassValid.length < 8) {
     alert("Password is Too Short");
+  } else {
+    alert("Perfect");
+    isGood += 1
+  }
+  if (itIncludes(isPassValid)) {
+    alert("Please add more symbols and letters.");
+  }
+  itBig(isPassValid);
+  if (isItTrue == false) {
+    alert("Add a UpperCase Letter");
   }
 }
-
+         
 btn.addEventListener("click", function () {
   isValidEmail();
   isValidPhoneNum();
   isValidPass();
+  if(isGood == 3){
+    window.location.href = '/TodoList/index.html'
+  }
 });
 
 checkBox.onclick = isVisible;
@@ -50,5 +69,28 @@ function isVisible() {
     show.type = "text";
   } else {
     show.type = "password";
+  }
+}
+
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+function itIncludes(PhoneNum) {
+  for (let i = 0; i < PhoneNum; i++) {
+    if (numbers.includes(Number(PhoneNum)[i])) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
+
+function itBig(Pass) {
+  let Upper = "";
+  for (let i = 0; i < Pass.length; i++) {
+    if (!itIncludes(Pass[i])) {
+      Upper = Pass[i].toUpperCase();
+      if (Upper == Pass[i]) {
+        isItTrue = true;
+      }
+    }
   }
 }
